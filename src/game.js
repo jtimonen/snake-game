6d00b223f960adfae1d68cc59f75ccb9d6d08289
jtimonen <juho.timonen@iki.fi>
2023-06-03 23:55:47 +0300

    improve end and restarting the game

--- src/game.js
+++ src/game.js
@@ -1,14 +1,30 @@
 import { gameBoard, blockSize, gameBoardSize, gameVariables} from './variables.js';
 
-export function updateLastDirection(dir) {
-  gameVariables.lastMovedDirection = dir;
+
+export function startGame() {
+  clearTimeout(gameVariables.gameLoopId);
+  resetGame();
+  document.getElementById('score').innerText = 'Score: 0';
+  document.getElementById('main-menu').style.display = 'none';
+  gameVariables.gameLoopId = setTimeout(gameLoop, 200);
 }
 
-export function eatApple() {
-  gameVariables.apple = null;
-  gameVariables.score += 1;
+export function gameOver() {
+  clearTimeout(gameVariables.gameLoopId);
+  alert("Game Over");
+  document.getElementById('main-menu').style.display = 'block';
   document.getElementById('score').innerText = "Score: " + gameVariables.score;
+  resetGame();
 }
+
+function resetGame() {
+  gameVariables.direction = 'Right';
+  gameVariables.lastMovedDirection = null;
+  gameVariables.snake = [{ top: 0, left: 0 }];
+  gameVariables.apple = null;
+  gameVariables.score = 0;
+}
+
 
 export function updateSnake() {
   var head = Object.assign({}, gameVariables.snake[0]); 
@@ -25,35 +41,57 @@ export function updateSnake() {
     gameVariables.snake.pop();
   }
 
-  if(isSnakeHittingItself(head)) {
-    return gameOver();
+  if(doesSnakeHitItself(head)) {
+    return true;  // return true when game over
   }
+  return false;  // return false otherwise
 }
 
-export function isSnakeHittingItself(head) {
-  for(let i = 1; i < gameVariables.snake.length; i++) {
-    if(gameVariables.snake[i].top === head.top && gameVariables.snake[i].left === head.left) {
+export function gameLoop() {
+  if(updateSnake()){  // if updateSnake() returns true
+    return gameOver();  // return immediately without scheduling a new loop
+  }
+  
+  updateApple();
+  drawGame();
+  gameVariables.gameLoopId = setTimeout(gameLoop, 200);
+  updateLastDirection(gameVariables.direction);
+}
+
+export function updateLastDirection(dir) {
+  gameVariables.lastMovedDirection = dir;
+}
+
+
+
+function moveSnakeHead(head) {
+  if (gameVariables.direction === 'Right') head.left = (head.left + blockSize) % gameBoardSize;
+  if (gameVariables.direction === 'Down') head.top = (head.top + blockSize) % gameBoardSize;
+  if (gameVariables.direction === 'Left') head.left = (head.left - blockSize + gameBoardSize) % gameBoardSize;
+  if (gameVariables.direction === 'Up') head.top = (head.top - blockSize + gameBoardSize) % gameBoardSize;
+}
+
+function isSnakeEatingApple(head) {
+  return gameVariables.apple && gameVariables.apple.top === head.top && gameVariables.apple.left === head.left;
+}
+
+export function eatApple() {
+  gameVariables.apple = null;
+  gameVariables.score += 1;
+  document.getElementById('score').innerText = "Score: " + gameVariables.score;
+}
+
+export function doesSnakeHitItself(head) {
+  for (let i = 1; i < gameVariables.snake.length; i++) {
+    if (gameVariables.snake[i].top === head.top && gameVariables.snake[i].left === head.left) {
       return true;
     }
   }
   return false;
 }
 
-export function gameOver() {
-  clearTimeout(gameVariables.gameLoopId);
-  alert("Game Over");
-  document.getElementById('main-menu').style.display = 'block';
-  document.getElementById('gameVariables.score').innerText = "Score: " + gameVariables.score;
-  
-  gameVariables.direction = 'Right';
-  gameVariables.lastMovedDirection = null;
-  gameVariables.snake = [{ top: 0, left: 0 }];
-  gameVariables.apple = null;
-  gameVariables.score = 0;
-}
-
 export function updateApple() {
-  if(gameVariables.apple === null) {
+  if (gameVariables.apple === null) {
     let newApplePosition;
     do {
       newApplePosition = {
@@ -66,24 +104,23 @@ export function updateApple() {
 }
 
 export function isAppleInsideSnake(position) {
-  for(let i = 0; i < gameVariables.snake.length; i++) {
-    if(gameVariables.snake[i].top === position.top && gameVariables.snake[i].left === position.left) {
+  for (let i = 0; i < gameVariables.snake.length; i++) {
+    let snakeSegment = gameVariables.snake[i];
+    if (snakeSegment.top === position.top && snakeSegment.left === position.left) {
       return true;
     }
   }
   return false;
 }
 
-export function gameLoop() {
-  updateSnake();
-  updateApple();
-  drawGame();
-  gameVariables.gameLoopId = setTimeout(gameLoop, 200);
-  updateLastDirection(gameVariables.direction);
-}
 
 export function drawGame() {
   gameBoard.innerHTML = '';
+  drawSnake();
+  drawApple();
+}
+
+function drawSnake() {
   gameVariables.snake.forEach(function(block) {
     var blockElem = document.createElement('div');
     blockElem.style.top = `${block.top}px`;
@@ -93,22 +130,13 @@ export function drawGame() {
     blockElem.style.border = '1px solid black';
     gameBoard.appendChild(blockElem);
   });
+}
 
+function drawApple() {
   var appleElem = document.createElement('div');
   appleElem.style.top = `${gameVariables.apple.top}px`;
   appleElem.style.left = `${gameVariables.apple.left}px`;
   appleElem.className = 'block';
   appleElem.style.background = 'red';
   gameBoard.appendChild(appleElem);
-}
-
-export function startGame() {
-  gameVariables.direction = 'Right';
-  gameVariables.lastMovedDirection = null;
-  gameVariables.snake = [{ top: 0, left: 0 }];
-  gameVariables.apple = null;
-  gameVariables.score = 0;
-  document.getElementById('score').innerText = 'Score: 0';
-  document.getElementById('main-menu').style.display = 'none';
-  gameVariables.gameLoopId = setTimeout(gameLoop, 200);
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { gameBoard, blockSize, gameBoardSize, validDirections } from './variables.js';
 import { gameVariables } from './variables.js';
 import { changeDirection } from './input.js';
-import { updateLastDirection, eatApple, updateSnake, isSnakeHittingItself, gameOver, updateApple, isAppleInsideSnake, gameLoop, drawGame} from './game.js';
 
 import { startGame } from './game.js';
 document.getElementById('start-button').addEventListener('click', startGame);
